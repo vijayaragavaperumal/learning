d5ab989ae90c497f9f42bd016eef78b68c937686
Vijayaragavaperumal <vijayaragavaperumal@skava.com>
2019-03-28 15:56:51 +0530

    code updated

--- DateTimeCheck.java
+++ DateTimeCheck.java
@@ -1,12 +1,5 @@
 /*******************************************************************************
- * Copyright ©2002-2018 Skava.
- * All rights reserved.The Skava system, including
- * without limitation, all software and other elements
- * thereof, are owned or controlled exclusively by
- * Skava and protected by copyright, patent, and
- * other laws. Use without permission is prohibited.
- *
- * For further information contact Skava at info@skava.com.
+ * Date Time Check
  ******************************************************************************/
 package com.skava.pricingadmin.web;
 

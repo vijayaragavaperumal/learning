aad50cd420fe7ec245ece8fafb0b0c2ad96691d1
Vijayaragavaperumal <vijayaragavaperumal@skava.com>
2019-05-14 18:20:19 +0530

    comment updated

--- myPackage/combile.bat
+++ myPackage/combile.bat
@@ -1,3 +1,7 @@
+::combile using absolute path
+::C:\Users\vijayaragavaperumal\Desktop\myPackage>javac -d C:\Users\vijayaragavaperumal\Desktop\myPackage Test.java
+::C:\Users\vijayaragavaperumal\Desktop\myPackage>javac -d C:\Users\vijayaragavaperumal\Desktop\myPackage Hello.java
+
 javac -d . Test.java
 javac -d . Hello.java
 

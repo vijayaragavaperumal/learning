536e1b2fef5f7ae60e2e7282eaa8d9c6076add28
vijayaragavaperumal <vijayaragavaperumal@skava.com>
2020-07-14 11:04:45 +0530

    update

--- JAVA_Learn/javalearn1/src/learn/java/JavaLearn.java
+++ JAVA_Learn/javalearn1/src/learn/java/JavaLearn.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright ©2002-2018 Skava.
- * All rights reserved.The Skava system, including
- * without limitation, all software and other elements
- * thereof, are owned or controlled exclusively by
- * Skava and protected by copyright, patent, and
- * other laws. Use without permission is prohibited.
- *
- * For further information contact Skava at info@skava.com.
- ******************************************************************************/
 package learn.java;
 
 import java.util.Scanner;
